2e336bf3df6de46f4a931a9a23b7642dd5c1ec9c
ojochegbe <peterspraisezy>
2025-05-30 21:22:28 +0000

    removed banner

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <div className="pt-16">
-          {" "}
           {children}
         </div>
         <Footer />
